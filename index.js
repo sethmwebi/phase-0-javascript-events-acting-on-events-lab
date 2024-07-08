a516ad9836045d144c43a29278c1909129e59fbe
Seth Mwebi <sethmwebi27@gmail.com>
2024-07-08 08:42:17 +0300

    adds code for javascript event listeners for phase-0-javascript-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
